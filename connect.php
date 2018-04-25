3bdd46856e54ade8c38e803bb8ea679ce6309b00
jleong8 <jleong8@hawaii.edu>
2018-04-25 01:10:11 -1000

    added backend settings

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
 
-    $servername = "localhost";
-    $username = "jleong8_jleong88";
-    $password = "22331387";
-    $dbname = "jleong8_missileAlert";
-    
+    $servername = "us-cdbr-iron-east-05.cleardb.net";
+    $username = "bd1dd24430dbcd";
+    $password = "612bccc8";
+    $dbname = "heroku_91135ca3bf7551b";
+
      try {
         $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        
+
     }
     catch(PDOException $e)
         {
